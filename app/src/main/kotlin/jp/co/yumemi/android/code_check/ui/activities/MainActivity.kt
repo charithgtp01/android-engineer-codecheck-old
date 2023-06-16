1395520f2171f3f70ef548e5c616fd1af7dc917b
Charith <vinodyav@tellko.com>
2023-06-16 17:41:51 +0530

    Rename files Create Project folder structure Create Separate files for adapters and entities Rename Fragments

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/activities/MainActivity.kt
@@ -1,12 +1,13 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.code_check.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
+import jp.co.yumemi.android.code_check.R
 import java.util.*
 
-class TopActivity : AppCompatActivity(R.layout.activity_top) {
+class MainActivity : AppCompatActivity(R.layout.activity_top) {
 
     companion object {
         lateinit var lastSearchDate: Date
